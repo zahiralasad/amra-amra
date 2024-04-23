85b907c4f276ceffe4441e61e41390687519995a
Zahir Al-Asad <zahiralasad@yahoo.com>
2024-04-23 12:00:30 +0200

    updated numbers

--- src/components/Picnics/Picnic2024.js
+++ src/components/Picnics/Picnic2024.js
@@ -125,6 +125,12 @@ function Picnic2024() {
                 <option value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
                 <option value="10">10</option>
               </select>
               <p className="ms-2">(275kr/child)</p>
@@ -140,6 +146,12 @@ function Picnic2024() {
                 <option value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
                 <option value="10">10</option>
               </select>
               <p className="ms-2">(2455kr/child)</p>
@@ -155,6 +167,12 @@ function Picnic2024() {
                 <option value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
                 <option value="10">10</option>
               </select>
               <p className="ms-2">(0kr/child)</p>
