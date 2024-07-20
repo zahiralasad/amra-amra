904b1eb5850c422ec490735f7cc7d838e56e7c48
zahiralasad <zahiralasad@yahoo.com>
2024-07-20 02:29:45 +0200

    added table in the checklist component

--- src/components/Checklist.js
+++ src/components/Checklist.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Table from 'react-bootstrap/Table';
 
 function Checklist() {
     //const url = "https://script.google.com/macros/s/AKfycbyHlPtLBBEoWFs3aKPqf4xM1X2GW9XS8aPDUPePdXUBXVJVSVNO2vNP8VCeHi3U5P-_-w/exec";
-    const url ="https://script.google.com/macros/s/AKfycby0GPcRIXtO3Pi1MK4u9AcoklsypIvoz1u1ZTw2om9JX_8dct5rS2bhA8WlPAWYr15q/exec";
+    const url ="https://script.google.com/macros/s/AKfycbxFZGnYUV660OpWKLJ03ZITJPeJMYcPZeIMKVAeVj0oI8YMJt-JHt42KDaadqOwHolS/exec";
     // const apiUrl = "https://amra-amra.se/emailApi/";
     const [entries, setEntries] = useState([]);
     const [error, setError] = useState(null);
@@ -15,7 +16,7 @@ function Checklist() {
             .then(response => {
                 setEntries(response.data);
                 setLoading(false);
-                console.log(response.data);
+                //console.log(response.data);
             })
             .catch(error => {
                 setError(error);
@@ -23,12 +24,27 @@ function Checklist() {
                 console.log(error);
             })
     });
-
     // if (loading) return <p className='test-white'>Loading ...</p>
     // if (error) return <p className='test-white'>Error: {error.message}</p>
     return (
         <div className="text-center text-white">
             checklist
+            <Table striped bordered hover>
+                <thead>
+                    <th>Entry No</th>
+                    <th>Adults</th>
+                    <th>BigKids</th>
+                    <th>SmallKids</th>
+                    <th>Babies</th>
+                </thead>
+            {entries.map((entry) => 
+                <tr>
+                    <th>{entry.entryNo}</th>
+                    <th>{entry.adults}</th>
+                </tr>
+                
+            )}
+            </Table>
             {/* <div>
                 <h1>Picnic 2024 Entries</h1>
                 <ul>
