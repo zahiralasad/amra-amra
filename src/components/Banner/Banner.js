c8efbad20caca3a881f913173761d53c4729eb17
Al-Asad Zahir <zahir.al-asad@scania.com>
2024-12-18 14:59:55 +0100

    ad bar done

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -5,17 +5,21 @@ import logo from './logo_122x100.jpg';
 function Banner() {
     return (
         <>
-            <div class="Banner">
-                <div class="Banner-left-column">
-                    <img class="Banner-top-row" src={logo} />
-                    <div class="Banner-bottom-row">
+            <div className="Banner">
+                <div className="Banner-left-column">
+                    <img className="Banner-top-row" src={logo} />
+                    <div className="Banner-bottom-row">
                         <h5>আমরা-আমরা</h5>
-                        <h7> সুইডেনে বসবাসরত বাংলাদেশী একটি অলাভজনক সামাজিক সংগঠন</h7>
+                        <h7> সুইডেনে বসবাসরত বাংলাদেশীদের নিয়ে গঠিত একটি অলাভজনক সংগঠন যার লক্ষ্য দেশীয় সংস্কৃতি চর্চা করা</h7>
                     </div>
                 </div>
-                <div class="Banner-right-column"></div>
+                <div className="Banner-right-column">
+                    {/* <div className="banner-item"></div> */}
+                </div>
             </div>
-            <div className="Ad-field"></div>
+            <div className="Ad-field">
+                <h5>New event coming soon</h5>
+            </div>
         </>
     )
 }
