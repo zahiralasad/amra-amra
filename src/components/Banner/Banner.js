2c12e74806a5435240d13c46092ad50ee2875ef1
Al-Asad Zahir <zahir.al-asad@scania.com>
2024-12-21 01:44:52 +0100

    continuing

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -9,21 +9,23 @@ function Banner() {
             <div className="row Banner" id="bigscreen">
 
                 <div className="col-1 Banner-left-column">
-                    <img className="Banner-top-row" src={logo} />
+                    <img className="Banner-top-row" style={{background: "black"}} src={logo} />
                     <div className="Banner-bottom-row">
                         <h5>আমরা-আমরা</h5>
                         <h7> সুইডেনে বসবাসরত বাংলাদেশীদের নিয়ে গঠিত একটি অলাভজনক সংগঠন যার লক্ষ্য দেশীয় সংস্কৃতি চর্চা করা</h7>
                     </div>
                 </div>
 
-                <div className="col-7 Banner-right-column">
+                <div className="col Banner-right-column">
                     {/* <div className="banner-item"></div> */}
 
+                </div>
+                <div style={{left: "230px"}}>
                     <div className="Banner-item" style={{ left: "27%", top: "180px" }}></div>
                     <div className="Banner-item" style={{ left: "45%", top: "150px" }}></div>
                     <div className="Banner-item" style={{ left: "63%", top: "120px" }}></div>
                     <div className="Banner-item" style={{ left: "81%", top: "90px" }}></div>
-                </div>
+                    </div>
             </div>
             <div className="row Ad-field">
                 <h5>New event coming soon</h5>
