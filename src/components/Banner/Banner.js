1de7e183cce55a1ea6ed1bb65c193b956dc4fb1b
Zahir Al-Asad <zahiralasad@yahoo.com>
2025-01-14 02:52:05 +0100

    started working on different screen

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -3,19 +3,25 @@ import './banner.css';
 import logo from './logo_122x100.jpg';
 
 function Banner() {
-    // console.log(window.innerWidth);
     return (
-        <div className="row Banner" id="bigscreen">
-            <div className="col-1 Banner-left-column">
-                <div className="Banner-top-row" style={{background: "black"}} src={logo}></div>
-                <div className="Banner-bottom-row">
-                <h5>আমরা-আমরা</h5>
+        <>
+            <div className="Banner">
+                <div className="Banner-left-column">
+                    <img className="Banner-top-row" src={logo} />
+                    <div className="Banner-bottom-row">
+                        <h5>আমরা-আমরা</h5>
                         <h7> সুইডেনে বসবাসরত বাংলাদেশীদের নিয়ে গঠিত একটি অলাভজনক সংগঠন যার লক্ষ্য দেশীয় সংস্কৃতি চর্চা করা</h7>
+                    </div>
+                </div>
+                <div className="Banner-right-column">
+                    {/* <div className="banner-item"></div> */}
                 </div>
             </div>
-            <div className="col Banner-right-column">right</div>
-        </div>
-
+            <div className="Ad-field">
+                <h5>New event coming soon</h5>
+            </div>
+        </>
     )
 }
+
 export default Banner;
--- src/components/Banner/Banner_old.js
+++ src/components/Banner/Banner_old.js
@@ -7,9 +7,9 @@ function Banner() {
     return (
         <div className="container">
             <div className="row Banner" id="bigscreen">
-
                 <div className="col-1 Banner-left-column">
-                    <img className="Banner-top-row" style={{background: "black"}} src={logo} />
+                    {/* <img className="Banner-top-row" style={{background: "black"}} src={logo} /> */}
+                    <div className="Banner-top-row"></div>
                     <div className="Banner-bottom-row">
                         <h5>আমরা-আমরা</h5>
                         <h7> সুইডেনে বসবাসরত বাংলাদেশীদের নিয়ে গঠিত একটি অলাভজনক সংগঠন যার লক্ষ্য দেশীয় সংস্কৃতি চর্চা করা</h7>
