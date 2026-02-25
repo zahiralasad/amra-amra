e086513b033d4ff876c50320dac75c21279bd7c0
Zahir Al-Asad <zahiralasad@yahoo.com>
2026-02-25 14:11:26 +0100

    showing the members

--- src/components/Members/Members.js
+++ src/components/Members/Members.js
@@ -1,16 +1,72 @@
 import React from 'react';
+import { useState, useEffect } from "react";
+
+
+const url = "https://script.google.com/macros/s/AKfycbw3zL12yxAeHhFubKpPNm2DXIeINp7_RZYPij4oKjiBzRUuY6aVEFFUCdBQugIeUsMljg/exec";
 
 function Members() {
+
+    const [members, setMembers] = useState([]);
+    const [ids, setIds] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState("");
+
+    useEffect(() => {
+        getMembers();
+    }, []);
+
+    function getMembers() {
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                setLoading(false);
+                setMembers(data.names);
+                setIds(data.ids);
+                console.log(data.names)
+            })
+            .catch(error => {
+                setError(error.message);
+                setLoading(false);
+            });
+    }
+
     return (
         <div className='row'>
             <div className="mt-4 text-center text-white">
                 <div>
                     <h6>
-                        <a href="registermember">Register</a> to be a member of the Amra-Amra association
+                        <div>Want to become a member of Amra-Amra?</div>
+                        <a href="registermember" className="btn btn-primary">Register</a> 
+                        
                     </h6>
                 </div>
                 <div className='pt-5'>
-                    <h6>Members</h6>
+                    <h6>Amra-Amra Members</h6>
+                    {loading && <div className="text-center  text-white my-5">Please wait while loading the from .........</div>}
+                    {error && <div className="text-center  text-white my-5">Error: {error}</div>}
+                    {!loading && !error && (
+                        <div className="p-2 border border-white rounded text-center">
+                            <table className='table table-striped table-dark'>
+                                <thead>
+                                    <tr>
+                                        <th>Id</th>
+                                        <th>Names</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {members.map((member, index) => {
+                                        const id = ids[index];
+                                        return (
+                                            <tr key={index}>
+                                                <td>{id}</td>
+                                                <td>{member}</td>
+                                            </tr>
+                                        )
+                                    })}
+                                </tbody>
+                            </table>
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
